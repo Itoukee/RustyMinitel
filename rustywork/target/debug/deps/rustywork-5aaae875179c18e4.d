fc0d0c259a93aa2d793cec77fbf893b1d3cef317
pauche_m <pauche_m@etna-alternance.net>
2022-03-29 09:20:20 +0200

    Add gitignore

---
+++ rustywork/target/debug/deps/rustywork-5aaae875179c18e4.d
@@ -0,0 +1,5 @@
+/home/matheo/Bureau/RustyMinitel/rustywork/target/debug/deps/rustywork-5aaae875179c18e4.rmeta: src/main.rs
+
+/home/matheo/Bureau/RustyMinitel/rustywork/target/debug/deps/rustywork-5aaae875179c18e4.d: src/main.rs
+
+src/main.rs:
---
+++ rustywork/target/debug/deps/rustywork-820da3514cfd0755.d
@@ -0,0 +1,5 @@
+/home/matheo/Bureau/RustyMinitel/rustywork/target/debug/deps/rustywork-820da3514cfd0755.rmeta: src/main.rs
+
+/home/matheo/Bureau/RustyMinitel/rustywork/target/debug/deps/rustywork-820da3514cfd0755.d: src/main.rs
+
+src/main.rs:
